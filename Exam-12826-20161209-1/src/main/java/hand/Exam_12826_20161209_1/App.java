5a49d061f79846ff4176fbbbcadbb35012af0194
武正艳 <zhengyan.wu@hand-china.com>
2016-12-09 20:49:56 +0800

    commit

---
+++ Exam-12826-20161209-1/src/main/java/hand/Exam_12826_20161209_1/App.java
@@ -0,0 +1,13 @@
+package hand.Exam_12826_20161209_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
